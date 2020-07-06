647a371bab892b4ad687a38a3df8e9373083fa79
ryota.nakayama <true.saori@gmail.com>
2020-07-06 02:47:27 +0000

    add restaurant_ranking_csv

--- roboter/models/robot.py
+++ roboter/models/robot.py
@@ -1,3 +1,4 @@
+from roboter.models import ranking
 from roboter.views import console
 
 
@@ -27,6 +28,7 @@ class Robot(object):
 class RestaurantRobot(Robot):
   def __init__(self, name=DEFAULT_ROBOT_NAME):
     super().__init__(name=name)
+    self.ranking_model = ranking.RankingModel()
   
   
   def _hello_decorator(func):
@@ -35,6 +37,32 @@ class RestaurantRobot(Robot):
         self.hello()
       return func(self)
     return wrapper
+    
+    
+  @_hello_decorator
+  def recommend_restaurant(self):
+    new_recommend_restaurant = self.ranking_model.get_most_popular()
+    if not new_recommend_restaurant:
+      return None
+      
+    will_recommend_restaurants = [new_recommend_restaurant]
+    while True:
+      template = console.get_template("greeting.txt", self.speak_color)
+      is_yes = input(template.substitute({
+        "robot_name": self.name,
+        "user_name": self.user_name,
+        "restaurant": new_recommend_restaurant,
+      }))
+      
+      if is_yes.lower() == "y" or is_yes.lower() == "yes":
+        break
+      
+      if is_yes.lower() == "n" or is_yes.lower() == "no":
+        new_recommend_restaurant = self.ranking_model.get_most_popular(
+          not_list=will_recommend_restaurants)
+        if not new_recommend_restaurant:
+          break
+        will_recommend_restaurants.append(new_recommend_restaurant)
       
       
   @_hello_decorator
@@ -48,7 +76,7 @@ class RestaurantRobot(Robot):
       }))
       
       if restaurant:
-        self.restaurant = restaurant
+        self.ranking_model.increment(restaurant)
         break
       
       
